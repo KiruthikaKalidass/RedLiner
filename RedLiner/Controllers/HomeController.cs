b2906a0aa5f08bdda15d51f3f934fa6c09adbd37
Kiruthika Kalidass <Kiruthika.Kalidass@GISBIZ-CHENNAI.COM>
2019-03-12 13:33:50 +0530

    RedLiner_Project Commit

--- RedLiner/Controllers/HomeController.cs
+++ RedLiner/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RedLiner.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,33 @@ namespace RedLiner.Controllers
 {
     public class HomeController : Controller
     {
+        ProjectDB empDB = new ProjectDB();
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        public JsonResult List()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            return Json(empDB.ListAll(), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Add(Project emp)
+        {
+            return Json(empDB.Add(emp), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetbyID(int Id)
+        {
+            var Employee = empDB.ListAll().Find(x => x.Id.Equals(Id));
+            return Json(Employee, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Update(Project emp)
+        {
+            return Json(empDB.Update(emp), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Delete(int Id)
+        {
+            return Json(empDB.Delete(Id), JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
